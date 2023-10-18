fb2f2bcf96f995c9dcccd7a4388ed36189d01b31
JulioKrack <julio.ruiz.dev@gmail.com>
2023-10-18 00:34:19 -0500

    se puso estados al index de reservas

--- secciones/reservas/index.php
+++ secciones/reservas/index.php
@@ -3,7 +3,7 @@ include("../../config/bd.php");
 
 // Función para obtener todas las reservas de citas desde la base de datos
 function getReservaciones($conn) {
-    $sql = "SELECT id,fechareserva,hora,asunto,(SELECT nombre FROM persona WHERE id=(SELECT id_persona FROM cliente where id=id_cliente)) as clientes,(SELECT nombre FROM persona WHERE id=(SELECT id_persona FROM veterinario where id=id_veterinario)) as veterinario, estado FROM reservadecitas";
+    $sql = "SELECT id,fechareserva,hora,asunto,(SELECT nombre FROM persona WHERE id=(SELECT id_persona FROM cliente where id=id_cliente)) as clientes,(SELECT nombre FROM persona WHERE id=(SELECT id_persona FROM veterinario where id=id_veterinario)) as veterinario, (CASE WHEN estado = 1 THEN 'Disponible' ELSE 'Ocupado' END ) as estado1 FROM reservadecitas";
     $result = $conn->query($sql);
 
     if ($result->num_rows > 0) {
@@ -62,7 +62,7 @@ $conn->close();
                     <td><?php echo $reservation['asunto']; ?></td>
                     <td><?php echo $reservation['clientes']; ?></td>
                     <td><?php echo $reservation['veterinario']; ?></td>
-                    <td><?php echo $reservation['estado']; ?></td>
+                    <td><?php echo $reservation['estado1']; ?></td>
                     <td>
                         <a href="editar.php?id=<?php echo $reservation['id']; ?>" class="btn btn-primary">Editar</a>
                         <a href="index.php?id=<?php echo $reservation['id']; ?>" class="btn btn-danger">Eliminar</a>
