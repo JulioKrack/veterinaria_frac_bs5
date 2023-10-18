76f93cb0d581aa0eedb95b8f222fd6be24c52ff4
JulioKrack <julio.ruiz.dev@gmail.com>
2023-10-17 23:49:51 -0500

    se modifico editar cliente/veterinario

---
+++ secciones/clientes/editar-anterior.php
@@ -0,0 +1,63 @@
+<?php include("../../plantillas/header.php")?>
+<?php
+include("../../config/bd.php");
+
+if(isset($_GET['id'])){
+    $id_cliente= $_GET['id'];
+    $sql = "SELECT * FROM cliente WHERE id = '$id_cliente'";
+    $result = $conn->query($sql);
+    $registro = $result->fetch_assoc();
+    $id = $registro['id'];
+    $id_persona = $registro['id_persona'];  
+
+}
+
+// Verificar si el formulario ha sido enviado
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $id= $_POST['id'];
+    $id_persona = $_POST["id_persona"];
+
+
+    $sql = "UPDATE cliente SET id_persona = '$id_persona' WHERE id = '$id'";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "modificado successfully";
+        header("Location:./index.php");
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
+
+$conn->close();
+?>
+    <div class="card">
+        <div class="card-header">
+            <p class="card-text">Formulario para crear una reserva.</p>
+        </div>
+        <div class="card-body">
+    <!-- Formulario para insertar datos -->
+            <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+                <div class="mb-3">
+                  <label for="id" class="form-label">ID:</label>
+                  <input type="text" readonly
+                    class="form-control" name="id" id="id" aria-describedby="helpId" value="<?php echo $id; ?>" disabled>
+                </div>
+                <div class="mb-3">
+                  <label for="id_persona" class="form-label">ID Persona:</label>
+                  <input type="text"
+                    class="form-control" name="id_persona" id="id_persona" aria-describedby="helpId" value="<?php echo $id_persona; ?>">
+                </div>
+
+
+                <button type="submit" class="btn btn-primary">Actualizar</button>                
+                <a href="index.php" class="btn btn-secondary">Regresar</a>
+            </form> 
+        </div>
+        <div class="card-footer text-muted">
+
+
+        </div>
+    </div>
+
+
+<?php include("../../plantillas/footer.php")?>
--- secciones/veterinario/editar.php
+++ secciones/veterinario/editar.php
@@ -4,24 +4,34 @@ include("../../config/bd.php");
 
 if(isset($_GET['id'])){
     $id_veterinario= $_GET['id'];
-    $sql = "SELECT * FROM veterinario WHERE id = '$id_veterinario'";
+    $sql = "SELECT * FROM persona WHERE id = (SELECT id_persona FROM veterinario where id = '$id_veterinario')";
     $result = $conn->query($sql);
     $registro = $result->fetch_assoc();
     $id = $registro['id'];
-    $id_persona = $registro['id_persona'];  
-
+    $nombre = $registro['nombre'];
+    $dni = $registro['dni'];
+    $correo = $registro['correo'];
+    $usuario = $registro['usuario'];
+    $contrasenia = $registro['contrasenia'];
+    $telefono = $registro['telefono'];
+    $rol = $registro['rol'];
+    $estado = $registro['estado'];
 }
-
 // Verificar si el formulario ha sido enviado
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $id= $_POST['id'];
-    $id_persona = $_POST["id_persona"];
+    $nombre = $_POST["nombre"];
+    $dni = $_POST["dni"];
+    $correo = $_POST["correo"];
+    $usuario = $_POST["usuario"];
+    $contrasenia = $_POST["contrasenia"];
+    $telefono = $_POST["telefono"];
+    $rol = 'Veterinario';
+    $estado = $_POST["estado"];
 
-
-    $sql = "UPDATE veterinario SET id_persona = '$id_persona' WHERE id = '$id'";
-
+    $sql = "UPDATE persona SET nombre = '$nombre', dni = '$dni', correo = '$correo', usuario = '$usuario', contrasenia = '$contrasenia', telefono = '$telefono', rol = '$rol', estado = '$estado' WHERE id = '$id';";
+    
     if ($conn->query($sql) === TRUE) {
-        echo "modificado successfully";
         header("Location:./index.php");
     } else {
         echo "Error: " . $sql . "<br>" . $conn->error;
@@ -32,32 +42,66 @@ $conn->close();
 ?>
     <div class="card">
         <div class="card-header">
-            <p class="card-text">Formulario para crear una reserva.</p>
+            <p class="card-text">Formulario para editar un usuario.</p>
         </div>
         <div class="card-body">
     <!-- Formulario para insertar datos -->
             <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
                 <div class="mb-3">
                   <label for="id" class="form-label">ID:</label>
-                  <input type="text" readonly
-                    class="form-control" name="id" id="id" aria-describedby="helpId" value="<?php echo $id; ?>" disabled>
+                  <input type="text" readonly value="<?php echo $id; ?>"
+                    class="form-control" readonly name="id" id="id" aria-describedby="helpId">
+                <div class="mb-3">
+                  <label for="nombre" class="form-label">Nombre Completo:</label>
+                  <input type="text"  value="<?php echo $nombre; ?>"
+                    class="form-control" name="nombre" id="nombre" aria-describedby="helpId" >
                 </div>
                 <div class="mb-3">
-                  <label for="id_persona" class="form-label">ID Persona:</label>
-                  <input type="text"
-                    class="form-control" name="id_persona" id="id_persona" aria-describedby="helpId" value="<?php echo $id_persona; ?>">
+                  <label for="dni" class="form-label">DNI:</label>
+                  <input type="text" value="<?php echo $dni; ?>"
+                    class="form-control" name="dni" id="dni" aria-describedby="helpId" >
+                </div>  
+                <div class="mb-3">
+                  <label for="correo" class="form-label">Correo:</label>
+                  <input type="text" value="<?php echo $correo; ?>"
+                    class="form-control" name="correo" id="correo" aria-describedby="helpId" >
                 </div>
-
-
-                <button type="submit" class="btn btn-primary">Actualizar</button>                
+                <div class="mb-3">
+                  <label for="usuario" class="form-label">Usuario:</label>
+                  <input type="text" value="<?php echo $usuario; ?>"
+                    class="form-control" name="usuario" id="usuario" aria-describedby="helpId" >
+                </div>
+                <div class="mb-3">
+                  <label for="contrasenia" class="form-label">Contraseña:</label>
+                  <input type="text" value="<?php echo $contrasenia; ?>"
+                    class="form-control" name="contrasenia" id="contrasenia" aria-describedby="helpId" >
+                </div>
+                <div class="mb-3">
+                  <label for="telefono" class="form-label">Teléfono:</label>
+                  <input type="text" value="<?php echo $telefono; ?>"
+                    class="form-control" name="telefono" id="telefono" aria-describedby="helpId" >
+                </div>
+                <div class="mb-3">
+                    <label for="rol" class="form-label">Rol:</label>
+                    <select class="form-select form-select-lg" disabled name="rol" id="rol">
+                        
+                        <option  value="Cliente">Cliente</option>
+                        <option  value="Administrador">Administrador</option>
+                        <option selected value="veterinario">Veterinario</option>
+                    </select>
+                </div>
+                <div class="mb-3">
+                    <label for="estado" class="form-label">Estado:</label>
+                    <select class="form-select form-select-lg" name="estado" id="estado">
+                        <option selected value="1">Disponible</option>
+                        <option value="2">Inactivo</option>
+                    </select>
+                </div>
+                <button type="submit" class="btn btn-primary">Actualizar</button>
                 <a href="index.php" class="btn btn-secondary">Regresar</a>
-            </form> 
+            </form>      
         </div>
-        <div class="card-footer text-muted">
-
-
-        </div>
+        <div class="card-footer text-muted"></div>
     </div>
-
 
 <?php include("../../plantillas/footer.php")?>
