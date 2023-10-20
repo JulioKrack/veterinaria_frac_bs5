c4642cea32bda2aa3f1b26e434a110177342086f
DevFrankUTP <U19311061@utp.edu.pe>
2023-10-20 09:19:09 -0500

    agrege structura html

--- bienvenido.php
+++ bienvenido.php
@@ -1,10 +1,25 @@
 <?php include("./config/bd.php");?>
 <?php include("plantillas/header.php"); ?>
 
-<link rel="stylesheet" href="">
-<h1>Hola</h1>
-<h1>Hola mundo desde HTML</h1>
 
-<a name="" id="" class="btn btn-primary" href="reserva-cliente.php" role="button">Reservar Cita </a>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bienvenido.css">
+    <title>Document</title>
+
+</head>
+<body>
+    <h1>Hola</h1>
+    <h1>Hola mundo desde HTML</h1>
+
+    <a name="" id="" class="btn btn-primary" href="reserva-cliente.php" role="button">Reservar Cita </a>
+</body>
+</html>
+
+
+
 
 <?php include("plantillas/footer.php"); ?>
