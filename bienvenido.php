ce055732585dc7e6ccdc419c1442a6f05f0bb0a0
DevFrankUTP <U19311061@utp.edu.pe>
2023-10-20 00:49:32 -0500

    agrege mensaje

--- bienvenido.php
+++ bienvenido.php
@@ -1,6 +1,8 @@
 <?php include("./config/bd.php");?>
 <?php include("plantillas/header.php"); ?>
 
+<link rel="stylesheet" href="">
+<h1>Hola</h1>
 <h1>Hola mundo desde HTML</h1>
 
 <a name="" id="" class="btn btn-primary" href="reserva-cliente.php" role="button">Reservar Cita </a>
