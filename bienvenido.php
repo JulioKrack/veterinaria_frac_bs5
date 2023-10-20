94291b987e0c5a4f0640ae2b9f787e21eaadaee7
DevFrankUTP <U19311061@utp.edu.pe>
2023-10-20 09:40:07 -0500

    menu eliminado

--- bienvenido.php
+++ bienvenido.php
@@ -1,6 +1,4 @@
 <?php include("./config/bd.php");?>
-<?php include("plantillas/header.php"); ?>
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -10,16 +8,29 @@
     <link rel="stylesheet" href="css/bienvenido.css">
     <title>Document</title>
 
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous"> 
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.css" />  
+    <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.js"></script>  
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
 </head>
 <body>
-    <h1>Hola</h1>
-    <h1>Hola mundo desde HTML</h1>
+    <div class="container">
 
-    <a name="" id="" class="btn btn-primary" href="reserva-cliente.php" role="button">Reservar Cita </a>
+        <div class="m-5">
+            <h1>Reserva tu cita en nuestra veterinaria</h1>
+        
+
+        <a name="" id="" class="btn btn-primary" href="reserva-cliente.php" role="button">Reservar Cita </a>
+        </div> 
+    </div>
+
+    <?php include("plantillas/footer.php"); ?>
 </body>
 </html>
 
 
 
 
-<?php include("plantillas/footer.php"); ?>
