503f7ee715a1f99a86d24b5c6f00500aab89862b
DevFrankUTP <U19311061@utp.edu.pe>
2023-10-27 00:09:15 -0500

    Calculo de productos arreglado

--- productos.php
+++ productos.php
@@ -25,7 +25,7 @@
     <div class="container">
         <nav class="navbar navbar-expand-lg navbar-light">
             <div class="container-fluid">
-                <a class="navbar-brand" href="#">Veterinaria FRAC </a>
+                <a class="navbar-brand" href="#"> <h2>Veterinaria FRAC </h2></a>
 
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
