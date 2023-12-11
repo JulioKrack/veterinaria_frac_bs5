deb1faac516524188b35597fb9cf5bde6a77409a
JulioKrack <julio.ruiz.dev@gmail.com>
2023-12-11 14:14:06 -0500

    ya recupera usuarios con roles

--- src/secciones/reservas/index.php
+++ src/secciones/reservas/index.php
@@ -3,7 +3,20 @@ include("../../config/bd.php");
 
 // Función para obtener todas las reservas de citas desde la base de datos
 function getReservaciones($conn) {
-    $sql = "SELECT r.id, r.fechareserva, r.hora, r.asunto, p.nombre AS cliente, m.nombre AS mascota, v.nombre AS veterinario, CASE WHEN r.estado = 1 THEN 'Disponible' ELSE 'Ocupado' END AS estado1 FROM reservadecitas r JOIN persona p ON r.id_persona = p.id AND p.rol = 'Cliente' JOIN mascota m ON m.id_persona = p.id JOIN persona v ON r.id_persona = v.id AND v.rol = 'Veterinario';";
+    
+    $sql = "SELECT rc.id as id, rc.fechareserva as fecha, rc.asunto as asunto,
+    (SELECT nombre FROM cliente WHERE id = rc.id_cliente) as clientes,
+    (SELECT nombre FROM veterinario WHERE id = rc.id_veterinario) as veterinario,
+    (CASE WHEN rc.estado = 1 THEN 'Disponible' WHEN rc.estado= 2 THEN 'Reservado' WHEN rc.estado=3 THEN 'Atendido' ELSE 'Cancelado' END) as estado1,
+    (SELECT m.nombre FROM mascota m 
+     JOIN cliente c ON m.id_cliente = c.id
+     WHERE c.id = rc.id_cliente) as nombre_mascota,
+    (SELECT m.tipo FROM mascota m 
+     JOIN cliente c ON m.id_cliente = c.id
+     WHERE c.id = rc.id_cliente) as tipo_mascota
+    FROM reservadecitas rc ";
+
+
     $result = $conn->query($sql);
 
     if ($result->num_rows > 0) {
@@ -13,20 +26,30 @@ function getReservaciones($conn) {
     }
 }
 
-if(isset($_GET['id'])){
-    $id_reserva = $_GET['id'];
-    $sql = "DELETE FROM reservadecitas WHERE id = '$id_reserva'";
+function eliminarReserva($conn, $id) {
+    $sql = "DELETE FROM reservadecitas WHERE id = $id";
+
     if ($conn->query($sql) === TRUE) {
         header("Location:./index.php");
+        return true;
+
     } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
+        return false;
     }
 }
 
 // Obtener todas las reservas existentes
 $reservations = getReservaciones($conn);
 
+// Obtener el ID de la reserva desde el formulario
+if (isset($_GET['idr'])) {
+    $id = $_GET['idr'];
+    eliminarReserva($conn, $id);
+}
+
+
 // Cerrar la conexión después de obtener los datos
+
 $conn->close();
 ?>
 <?php include("../../plantillas/header.php")?>
@@ -45,10 +68,10 @@ $conn->close();
             <tr>
                 <th hidden>ID Reserva</th>
                 <th>Fecha Reservada</th>
-                <th>Hora</th>
-                <th>Asunto</th>
+                <th>Descripcion</th>
                 <th>Cliente</th>
-                <th>Mascota</th>
+                <th>Nombre Mascota</th>
+                <th>Tipo Mascota</th>
                 <th>Veterinario</th>
                 <th>Estado</th>
                 <th>Acciones</th>
@@ -57,20 +80,23 @@ $conn->close();
             <?php foreach ($reservations as $reservation) : ?>
                 <tr>
                     <td hidden><?php echo $reservation['id']; ?></td>
-                    <td><?php echo $reservation['fechareserva']; ?></td>
-                    <td><?php echo $reservation['hora']; ?></td>
+                    <td><?php echo $reservation['fecha']; ?></td>
                     <td><?php echo $reservation['asunto']; ?></td>
-                    <td><?php echo $reservation['clientes']; ?></td>
-                    <td><?php echo $reservation['mascota']; ?></td>
+                    <td ><?php echo $reservation['clientes']; ?></td>
+                    <td><?php echo $reservation['nombre_mascota']; ?></td>
+                    <td><?php echo $reservation['tipo_mascota']; ?></td>
                     <td><?php echo $reservation['veterinario']; ?></td>
                     <td><?php echo $reservation['estado1']; ?></td>
                     <td>
-                        <a href="editar.php?id=<?php echo $reservation['id']; ?>" class="btn btn-primary">Editar</a>
-                        <a href="index.php?id=<?php echo $reservation['id']; ?>" class="btn btn-danger">Eliminar</a>
+                        <form method="post" action="index.php">
+                            <input type="hidden" name="idr" value="<?php echo $reservation['id']; ?>">
+                            <button type="submit" class="btn btn-danger">Eliminar</button>
+                        </form>
                     </td>
                     </tr>
                     
             <?php endforeach; ?>
+
             </table>
         </div>
         
--- src/secciones/usuarios/index.php
+++ src/secciones/usuarios/index.php
@@ -1,11 +1,17 @@
 <?php
 include("../../config/bd.php");
 
-
-// Función para obtener todas las reservas de citas desde la base de datos
-function getDatosPersona($conn) {
-    $sql = "SELECT id, nombre, dni, correo, usuario, contrasenia, telefono, rol, (CASE WHEN estado = 1 THEN 'Activo' WHEN estado = 2 THEN 'Descanso/vacaciones' ELSE 'Baja' END) AS estados 
-    FROM persona;";
+// Función para obtener todos los usuarios desde la base de datos (veterinario, cliente y administrador)
+function getDatosUsuarios($conn) {
+    $sql = "SELECT id, nombre, dni, correo, usuario, contrasenia, estado, 'veterinario' as rol 
+            FROM veterinario
+            UNION
+            SELECT id, nombre, dni, correo, usuario, contrasenia, estado, 'cliente' as rol 
+            FROM cliente
+            UNION
+            SELECT id, nombre, dni, correo, usuario, contrasenia, estado, 'administrador' as rol 
+            FROM administrador";
+    
     $result = $conn->query($sql);
 
     if ($result->num_rows > 0) {
@@ -16,21 +22,22 @@ function getDatosPersona($conn) {
 }
 
 if(isset($_GET['id'])){
-    $id_persona= $_GET['id'];
+    $id_persona = $_GET['id'];
     $sql = "DELETE FROM persona WHERE id = '$id_persona'";
     if ($conn->query($sql) === TRUE) {
         header("Location:./index.php");
     } else {
-        echo "Error: Este usuario esta asignado con otras tablas " . $sql . "<br>" . $conn->error;
+        echo "Error: Este usuario está asignado con otras tablas " . $sql . "<br>" . $conn->error;
     }
 }
 
-// Obtener todas las reservas existentes
-$reservations = getDatosPersona($conn);
+// Obtener todos los usuarios existentes
+$usuarios = getDatosUsuarios($conn);
 
 // Cerrar la conexión después de obtener los datos
 $conn->close();
 ?>
+
 <?php include("../../plantillas/header.php")?>
 
 <br />
@@ -43,40 +50,39 @@ $conn->close();
     <div class="card-body">
         <div class="table-responsive-sm">
             <table class="table table-bordered" id="tabla_id">
-            <tr>
-                <th>ID Persona</th>
-                <th>Nombre</th>
-                <th>DNI</th>
-                <th>Correo</th>
-                <th>Usuario</th>
-                <th>Contraseña</th>
-                <th>Teléfono</th>
-                <th>Rol</th>
-                <th>Estado</th>
-            </tr>
-
-            <?php foreach ($reservations as $reservation) : ?>
                 <tr>
-                    <td><?php echo $reservation['id']; ?></td>
-                    <td><?php echo $reservation['nombre']; ?></td>
-                    <td><?php echo $reservation['dni']; ?></td>
-                    <td><?php echo $reservation['correo']; ?></td>
-                    <td><?php echo $reservation['usuario']; ?></td>
-                    <td><?php echo $reservation['contrasenia']; ?></td>
-                    <td><?php echo $reservation['telefono']; ?></td>
-                    <td><?php echo $reservation['rol']; ?></td>
-                    <td><?php echo $reservation['estados']; ?></td>
-                    <td>
-                        <a href="editar.php?id=<?php echo $reservation['id']; ?>" class="btn btn-primary">Editar</a>
-                        <a href="index.php?id=<?php echo $reservation['id']; ?>" class="btn btn-danger">Eliminar</a>
-                    </td>
+                    <th>ID Persona</th>
+                    <th>Nombre</th>
+                    <th>DNI</th>
+                    <th>Correo</th>
+                    <th>Usuario</th>
+                    <th hidden>Contraseña</th>
+                    <th>Rol</th>
+                    <th>Estado</th>
+                </tr>
+
+                <?php foreach ($usuarios as $usuario) : ?>
+                    <tr>
+                        <td><?php echo $usuario['id']; ?></td>
+                        <td><?php echo $usuario['nombre']; ?></td>
+                        <td><?php echo $usuario['dni']; ?></td>
+                        <td><?php echo $usuario['correo']; ?></td>
+                        <td><?php echo $usuario['usuario']; ?></td>
+                        <td hidden><?php echo $usuario['contrasenia']; ?></td>
+                        <td><?php echo $usuario['rol']; ?></td>
+                        <td><?php echo $usuario['estado']; ?></td>
+                        <td>
+                            <a href="editar.php?id=<?php echo $usuario['id']; ?>" class="btn btn-primary">Editar</a>
+                            <a href="index.php?id=<?php echo $usuario['id']; ?>" class="btn btn-danger">Eliminar</a>
+                        </td>
                     </tr>
-            <?php endforeach; ?>
+                <?php endforeach; ?>
+
             </table>
         </div>  
     </div>
     <div class="card-footer text-muted">
-    <a hidden href="crear.php?id=<?php echo $reservation['id']; ?>" class="btn btn-primary">Crear Persona</a>
+        <a hidden href="crear.php?id=<?php echo $usuario['id']; ?>" class="btn btn-primary">Crear Persona</a>
     </div>
 </div>
 
